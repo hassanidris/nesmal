49d4ee7df9d29e55eaccc64a6dc4840b722a72ae
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-28 13:15:39 +0200

    adjust files

--- src/pages/Egypt/index.js
+++
@@ -1,6 +0,0 @@
-import Egypt from "./Egypt";
-import EgyptAbout from "./EgyptAbout";
-import EgyptCompany from "./EgyptCompany";
-import EgyptContact from "./EgyptContact";
-
-export default { Egypt, EgyptAbout, EgyptCompany, EgyptContact };
