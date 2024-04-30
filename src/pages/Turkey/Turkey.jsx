654ff335dbcbf081d4e02a6fb82c10626f19f590
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-30 08:43:35 +0200

    Revert "fixing redirect"
    This reverts commit 43e1a23faa51f9a807657f78dcd68c805cbc1b54.

---
+++ src/pages/Turkey/Turkey.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { getTurkeyData } from "../../constants/data";
+import CompanyCard from "../../components/CompanyCard";
+import { Link } from "react-router-dom";
+import { turkeyBanner, turkeyHomeImage } from "../../constants/images";
+
+const Turkey = () => {
+  const dataT = getTurkeyData();
+  return (
+    <>
+      <div className="banner w-full h-[400px]">
+        <img
+          className=" h-full w-full object-cover"
+          src={turkeyBanner}
+          alt=""
+        />
+      </div>
+      <div className="wrapper max-w-[1290px] w-full m-auto mt-16 px-4">
+        <div className=" mb-10">
+          <h3 className="text-center text-3xl sm:text-[45px] tracking-[-1px] leading-relaxed">
+            EXPLORE NESMAL TURKEY
+          </h3>
+          <h2 className="text-center text-4xl sm:text-[50px] font-bold tracking-[-1px] leading-relaxed">
+            GROUP OF COMPANIES
+          </h2>
+          <hr className=" border-2 border-green-700" />
+        </div>
+        <div className=" flex flex-wrap justify-center items-center gap-6">
+          {dataT.map((company) => (
+            <Link to={`/turkey/company/${company.id}`} className="card">
+              <CompanyCard company={company} key={company.id} />
+            </Link>
+          ))}
+        </div>
+        <div className=" flex flex-col-reverse sm:flex-row gap-10 my-14">
+          <div className="left flex-1">
+            <h2 className=" text-3xl font-bold mb-4">About Nesmal Turkey</h2>
+            <div className=" text-xl mb-4 tracking-wide leading-relaxed">
+              <p>
+                We look forward to strengthening our reputation for successfully
+                carrying out unique investment opportunities, growing assets and
+                delivering strong returns to our investors and shareholders.
+              </p>
+              <p>
+                Nesmal Yatırım Holding A.Ş. was established in April 2012 as a
+                Turkey office of Nesma Holding Company. The aim of Nesmal
+                Yatirim Holding is to invest in dynamic sectors in Turkey and
+                establish a diversified strong investment portfolio.
+              </p>
+            </div>
+            <Link
+              className=" bg-green-600 text-white border border-green-600 py-2 px-5 hover:bg-transparent hover:text-green-600"
+              to="/turkey/turkey_about"
+            >
+              Read More
+            </Link>
+          </div>
+          <div className="right flex-1">
+            <img
+              className=" h-full w-full object-cover"
+              src={turkeyHomeImage}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Turkey;
---
+++ src/pages/Turkey/TurkeyCareer.jsx
@@ -0,0 +1,182 @@
+import React from "react";
+import { nesmalLogo } from "../../constants/images";
+import { NavLink } from "react-router-dom";
+
+const TurkeyCareer = () => {
+  return (
+    <>
+      <div className=" text-center p-12 bg-gray-300">
+        <h2 className=" text-6xl font-bold uppercase">join us</h2>
+        <p className=" capitalize mt-3 text-gray-500">
+          <NavLink
+            className=" underline text-gray-600 hover:text-gray-800"
+            to="/"
+          >
+            Home
+          </NavLink>
+          {" / "}
+          <NavLink
+            className=" underline text-gray-600 hover:text-gray-800"
+            to="/turkey"
+          >
+            turkey
+          </NavLink>{" "}
+          / career
+        </p>
+      </div>
+      <div className=" max-w-[1290px] w-full m-auto">
+        <div className="content flex flex-col sm:flex-row gap-10 px-4 my-16">
+          <div className="left flex-[3]">
+            <h1 className=" text-5xl leading-relaxed">Send Us Your CV</h1>
+            <p>
+              We can only accept CV / resume submissions online - by applying
+              your details below.
+            </p>
+            <p>
+              Any CV / resume submitted by email will not reach our recruiters.
+            </p>
+            <div>
+              <div>
+                <form>
+                  <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-12">
+                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-9">
+                        <div className="sm:col-span-3">
+                          <label
+                            for="full-name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Full Name
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              name="full-name"
+                              id="full-name"
+                              autocomplete="given-name"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                          <label
+                            for="mobile-number"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Mobile Number
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              name="mobile-number"
+                              id="mobile-number"
+                              autocomplete="mobile-number"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                          <label
+                            for="email"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Email address
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              id="email"
+                              name="email"
+                              type="email"
+                              autocomplete="email"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-span-full">
+                          <label
+                            for="cover-photo"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Upload CV
+                          </label>
+                          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                            <div className="text-center">
+                              <svg
+                                className="mx-auto h-12 w-12 text-gray-300"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                aria-hidden="true"
+                              >
+                                <path
+                                  fill-rule="evenodd"
+                                  d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                                  clip-rule="evenodd"
+                                />
+                              </svg>
+                              <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                <label
+                                  for="file-upload"
+                                  className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                                >
+                                  <span>Upload a file</span>
+                                  <input
+                                    id="file-upload"
+                                    name="file-upload"
+                                    type="file"
+                                    className="sr-only"
+                                  />
+                                </label>
+                                <p className="pl-1">or drag and drop</p>
+                              </div>
+                              <p className="text-xs leading-5 text-gray-600">
+                                PDF, DOC up to 10MB
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mt-6 flex items-center justify-end gap-x-6">
+                    <button
+                      type="submit"
+                      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Send CV
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="right flex-1">
+            <div className="logo w-36 m-auto mb-10">
+              <img src={nesmalLogo} />
+            </div>
+            <div className="address mt-10">
+              <h3 className=" text-4xl mb-2 border-b-2 pb-2 border-green-700">
+                Contact Us
+              </h3>
+              <h4 className=" font-bold">Nesmal Yatırım Holding A.Ş.</h4>
+              <p className=" text-gray-500">
+                Merdivenköy Mahallesi Bora Sokak, Nida Kule Göztepe, 25. kat,
+                No:92 / Kadıköy/ Istanbul
+              </p>
+              <p className=" text-gray-500">
+                <span className=" font-bold">Tel:</span> 0 216 343 02 40
+              </p>
+              <p className=" text-gray-500">
+                <span className=" font-bold">Fax:</span> 0 216 530 07 83
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TurkeyCareer;
